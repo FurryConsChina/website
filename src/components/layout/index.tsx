7c995c723218e26c3800c5817a6e3805c190db7f
JiPai <github@jipai.moe>
2025-06-28 23:09:49 +0800

    feat(icon): new favicon

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -118,6 +118,23 @@ export default function Layout({
         />
         <meta name="baidu-site-verification" content={"codeva-GHH5uUsoan"} />
         <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
         <link rel="canonical" href={`https://www.furrycons.cn${asPath}`} />
         <link
           rel="alternate"
