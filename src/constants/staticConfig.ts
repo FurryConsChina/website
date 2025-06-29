25ab48e40c4bf534d5d7ddeff82ac0760c3af388
JiPai <github@jipai.moe>
2025-06-29 22:45:27 +0800

    chore(track): add baidu track

--- src/constants/staticConfig.ts
+++ src/constants/staticConfig.ts
@@ -1,36 +1,36 @@
 export const FriendSiteLinks = [
   {
-    link: "https://www.wilddream.net/?utm_source=fec",
+    link: "https://www.wilddream.net/?utm_source=fcc",
     label: "WildDream创作站",
     description:
       "WildDream 创作站（野梦）是一个以原创作品分享为主的中文兽、兽人、动物、奇幻生物主题创作与交流站点，自 2015 年 8 月以来，为兽主题作品提供托管与展示服务。",
   },
   {
-    link: "https://srk.games/?utm_source=fec",
+    link: "https://srk.games/?utm_source=fcc",
     label: "兽人控游戏库 SRK.Games",
     description:
       "地表最强兽人控游戏索引！从传统角色扮演兽游到恋爱模拟，或是战略类兽人游戏，甚至更小众的选择，你都能在这里找到。",
   },
   {
-    link: "https://furrynovel.ink/?utm_source=fec&utm_campaign=friend_link",
+    link: "https://furrynovel.ink/?utm_source=fcc&utm_campaign=friend_link",
     label: "Linpx",
     description:
       "Linpx 是一个包含多种常用功能的 Pixiv 小说阅读器。Linpx 旨在帮助读者快速、方便地阅读 Pixiv 小说，有效地找到喜欢的作者和作品并帮助作者将作品带给更多读者。",
   },
   {
-    link: "https://fursuit.cool/?utm_source=fec&utm_campaign=friend_link",
+    link: "https://fursuit.cool/?utm_source=fcc&utm_campaign=friend_link",
     label: "FurSuit 绒兽志",
     description:
       "「绒兽志」致力于为您提供最新最全的毛毛图鉴资讯整合，快来这里看看吧！",
   },
   {
-    link: "https://www.furwall.net/?utm_source=fec&utm_campaign=friend_link",
+    link: "https://www.furwall.net/?utm_source=fcc&utm_campaign=friend_link",
     label: "绒毛墙 - 兽迷交流新天地",
     description:
       "让社群交流更亲密友善，还能精彩重温兽圈中每一个温馨与欢乐的瞬间，让这段旅程充满毛绒绒的爱与共鸣。",
   },
   {
-    link: "https://app.nyanket.cn/?utm_source=fec",
+    link: "https://app.nyanket.cn/?utm_source=fcc",
     label: "Nyanket 喵通贩",
     description:
       "Nyanket 喵通贩 是最早立足于提供 票务一体化解决方案 的服务商，我们与兽展的故事起源于兽夏祭。",
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -19,6 +19,24 @@ export default function Document() {
             src={"https://umami.abo.network/script.js"}
           />
         )}
+        {isEnableTrack && (
+          <Script
+            id="baidu-track"
+            type="text/javascript"
+            strategy="lazyOnload"
+            async
+          >
+            {`
+              var _hmt = _hmt || [];
+              (function() {
+                var hm = document.createElement("script");
+                hm.src = "https://hm.baidu.com/hm.js?e0423a9855401975d966aa65c0b2509f";
+                var s = document.getElementsByTagName("script")[0]; 
+                s.parentNode.insertBefore(hm, s);
+              })();
+              `}
+          </Script>
+        )}
       </body>
     </Html>
   );
