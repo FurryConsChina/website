f3a152e798d1d476c50945177d50ea3f8b2447b3
JiPai <github@jipai.moe>
2025-10-01 20:57:39 +0800

    feat(loader): use Qcloud CDN anyway

--- src/utils/imageLoader.ts
+++ src/utils/imageLoader.ts
@@ -13,10 +13,6 @@ export const getEventCoverImgPath = (event: Partial<EventType>) => {
 };
 
 export const imageUrl = (src: string) => {
-  if (process.env.NODE_ENV === "development") {
-    return `https://${GLOBAL_MANUAL_CDN_IMAGE_URL}/${src}`;
-  }
-
   return `https://${GLOBAL_AUTO_CDN_IMAGE_URL}/${src}`;
 };
 
@@ -33,10 +29,7 @@ const imageLoader = ({
   width?: number;
   height?: number;
 }) => {
-  const imageURLHost =
-    process.env.NODE_ENV === "development"
-      ? `https://${GLOBAL_MANUAL_CDN_IMAGE_URL}`
-      : `https://${GLOBAL_AUTO_CDN_IMAGE_URL}`;
+  const imageURLHost = `https://${GLOBAL_AUTO_CDN_IMAGE_URL}`;
   const imageURL = new URL(`${imageURLHost}/${src}`);
 
   width !== undefined && imageURL.searchParams.set("w", width.toString());
