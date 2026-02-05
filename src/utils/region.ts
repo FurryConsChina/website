9bd91e93b332b2539771fd829faf95f48d19af11
JiPai <github@jipai.moe>
2026-02-06 02:46:48 +0800

    style: restyle organization list

--- src/utils/region.ts
+++ src/utils/region.ts
@@ -2,5 +2,8 @@ import { Region } from "@/types/region";
 import { currentSupportLocale } from "@/utils/meta";
 
 export function getRegionName(region: Region, currentLocale: currentSupportLocale) {
-  return region.localName || region.name;
+  if (currentLocale === "zh-Hans") {
+    return region.name;
+  }
+  return region.localName;
 }
